ef624275e764f6c56204e910cd67e2ce7f66e26f
cdukcom <cduqueacero2016@gmail.com>
2025-06-19 12:41:39 -0500

    🔐 Añadir archivo .env.example para configuración segura

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAadBhE3K4QNodwhSriavgitb_pJG2wF7Q",
-  authDomain: "plataforma-iot-a3c52.firebaseapp.com",
-  projectId: "plataforma-iot-a3c52",
-  storageBucket: "plataforma-iot-a3c52.appspot.com",
-  messagingSenderId: "417994515846",
-  appId: "1:417994515846:web:4276f8e173d124ad77b807"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
